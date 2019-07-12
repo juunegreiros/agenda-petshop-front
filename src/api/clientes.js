46d60d543eed06f59c3352ac8255a5b5ba67513a
Ju <ju_bsnegreiros@hotmail.com>
2019-07-12 15:00:17 -0300

    Final vídeo 2.3

--- src/api/clientes.js
+++ src/api/clientes.js
@@ -1,29 +1,21 @@
-import { api } from './config'
+import { api, opcoesFetch } from './config'
 
-const listarClientes = () => 
-  api
-    .get('/clientes')
-    .then(resposta => resposta.data)
+const listarClientes = () =>
+  fetch('http://localhost:4000/', opcoesFetch('{clientes { nome cpf }}'))
+    .then(resposta => resposta.json())
+    .then(dados => dados.data.clientes)
 
-const buscarClientePorId = id => 
-  api
-    .get(`/clientes/cliente/${id}`)
-    .then(resposta => resposta.data[0])
+const buscarClientePorId = id =>
+  api.get(`/clientes/cliente/${id}`).then(resposta => resposta.data[0])
 
-const adicionarCliente = cliente => 
-  api
-    .post('/clientes/cliente', cliente)
-    .then(resposta => resposta.data)
+const adicionarCliente = cliente =>
+  api.post('/clientes/cliente', cliente).then(resposta => resposta.data)
 
 const alterarCliente = (id, cliente) =>
-  api
-    .put(`/clientes/cliente/${id}`, cliente)
-    .then(resposta => resposta.data)
+  api.put(`/clientes/cliente/${id}`, cliente).then(resposta => resposta.data)
 
-const removerCliente = id => 
-  api
-    .delete(`/clientes/cliente/${id}`)
-    .then(resposta => resposta.data)
+const removerCliente = id =>
+  api.delete(`/clientes/cliente/${id}`).then(resposta => resposta.data)
 
 export default {
   listarClientes,
--- src/api/config.js
+++ src/api/config.js
@@ -1,9 +1,19 @@
-import axios from 'axios'
+import axios from "axios";
 
 export const api = axios.create({
-  baseURL: 'http://localhost:4000',
-  timeout: 10000,
-  headers: {
-    'content-type': 'application/json',
-  }
+    baseURL: "http://localhost:5000",
+    timeout: 10000,
+    headers: {
+        "content-type": "application/json"
+    }
+});
+
+export const opcoesFetch = (query) => ({
+    method: 'post',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+        query
+    })
 })
