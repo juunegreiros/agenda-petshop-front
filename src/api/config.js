3fe992f19c50e7c80586ce9fc3c86f58582fca32
Ju <ju_bsnegreiros@hotmail.com>
2019-07-23 14:55:37 -0300

    Final aula 5.2

--- src/api/config.js
+++ src/api/config.js
@@ -1,19 +1,24 @@
-import axios from "axios";
+import axios from 'axios'
+import ApolloClient from 'apollo-boost'
 
 export const api = axios.create({
-    baseURL: "http://localhost:5000",
-    timeout: 10000,
-    headers: {
-        "content-type": "application/json"
-    }
-});
+  baseURL: 'http://localhost:5000',
+  timeout: 10000,
+  headers: {
+    'content-type': 'application/json',
+  },
+})
 
-export const opcoesFetch = (query) => ({
-    method: 'post',
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-        query
-    })
+export const opcoesFetch = query => ({
+  method: 'post',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({
+    query,
+  }),
+})
+
+export const apolloClient = new ApolloClient({
+  uri: 'http://localhost:4000',
 })
